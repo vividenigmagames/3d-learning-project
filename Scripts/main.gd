9a3772f28273edb9520e26edba59eae1d2baaf9f
vividenigmagames <vividenigmagames@gmail.com>
2025-12-11 01:13:38 -0800

    I goofed

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@onready var player = $player3d
 @onready var musicAudioStreamBG = $AudioStreamPlayerBGMusic
 var backgroundMusicOn = true
 
@@ -12,3 +13,6 @@ func update_music_status():
 			musicAudioStreamBG.play()
 	else:
 		musicAudioStreamBG.stop()
+		
+func _physics_process(delta: float) -> void:
+	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
